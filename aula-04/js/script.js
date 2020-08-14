9d7c7b3a1467da8bb2efafaef4fd0a8052bd0f00
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-14 19:57:44 -0300

    Aprendendo sobre classes no js

--- aula-04/js/script.js
+++ aula-04/js/script.js
@@ -2,4 +2,36 @@ class Animal {
     constructor(name) {
         this.name = name;
     }
+    speak(){
+        console.log(`${this.name}`);
+    }
 }
+
+class Dog extends Animal{
+    constructor(name, type){
+        super(name);
+        this.type = type;
+    }
+
+    speak(){
+        console.log(`${this.name} (${this.type}) latindo`);
+    }
+}
+
+class Cat extends Animal{
+    constructor(name, type){
+        super(name);
+        this.type =type;
+    }
+
+    speak(){
+        console.log(`${this.name} (${this.type}) miando`);
+    }
+}
+
+const animal = new Animal('toto');
+const dog = new Dog('bacon', 'vira-lata');
+const cat = new Cat('ted', 'vira-lata');
+animal.speak();
+dog.speak();
+cat.speak();
