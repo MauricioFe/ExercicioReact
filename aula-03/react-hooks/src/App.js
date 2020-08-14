2baa009d9606b92809d134c2d741cd65a861b1be
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-14 19:41:17 -0300

    Finalização do projeto com hooks

--- aula-03/react-hooks/src/App.js
+++ aula-03/react-hooks/src/App.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { getNewTimestamp } from './helpers/dateTimeHelpers';
 export default function App() {
 
   const [clickArray, setClickArray] = useState([])
+  useEffect(() =>{
+    document.title = clickArray.length;
+  })
   const handleClick = () => {
     const newClickArray = Object.assign([], clickArray);
     newClickArray.push(getNewTimestamp());
@@ -13,7 +16,7 @@ export default function App() {
       <h1>
         React e Hooks
       </h1>
-       <button onClick={handleClick}>Clique aqui</button> 
+       <button className="btn" onClick={handleClick}>Clique aqui</button> 
 
       <ul>
         {clickArray.map((item) => {
