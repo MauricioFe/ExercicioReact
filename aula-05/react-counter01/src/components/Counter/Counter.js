f4207fe26ebaa8ef00e51c8d408ed57dcc13c905
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-15 08:09:58 -0300

    Primeira parte dos counters em funcionamento

--- aula-05/react-counter01/src/components/Counter/Counter.js
+++ aula-05/react-counter01/src/components/Counter/Counter.js
@@ -4,18 +4,32 @@ export default class Counter extends Component {
     constructor() {
         super();
         this.state = {
-            currentCounter: 2
+            currentCounter: 2,
+            steps: 0,
         }
     }
-    handleClick = () => {
-        this.state--;
+    handleButtonDownClick = () => {
+        const { currentCounter, steps } = this.state;
+        this.setState({
+            currentCounter: currentCounter - 1,
+            steps: steps + 1
+        })
+    }
+    handleButtonUpClick = () => {
+        const { currentCounter, steps } = this.state;
+        this.setState({
+            currentCounter: currentCounter + 1,
+            steps: steps + 1
+        })
     }
     render() {
+        const { currentCounter, steps } = this.state;
         return (
             <div className={css.counterContainer}>
-                <button onClick={this.handleClick} className="waves-effect waves-light btn red darken-4">-</button>
-                <span className={css.counterValue}>{this.currentCounter}</span>
-                <button className="waves-effect waves-light btn green darken-4">+</button>
+                <button onClick={this.handleButtonDownClick} className="waves-effect waves-light btn red darken-4">-</button>
+                <span className={css.counterValue}>{currentCounter}</span>
+                <button onClick={this.handleButtonUpClick} className="waves-effect waves-light btn green darken-4">+</button>
+                <span className={css.counterValue}>({steps})</span>
             </div>
         )
     }
