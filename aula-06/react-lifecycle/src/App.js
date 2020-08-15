7a65ea2fd122af1fa51833a0066507a3797f8138
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-15 10:46:48 -0300

    Implementação da interface

--- aula-06/react-lifecycle/src/App.js
+++ aula-06/react-lifecycle/src/App.js
@@ -1,8 +1,42 @@
 import React, { Component } from 'react';
-import ProjetoBase from './components/ProjetoBase/ProjetoBase';
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+      showUsers: false
+    }
+  }
+  async componentDidMount() {
+    const res = await fetch('https://randomuser.me/api/?seed=rush&nat=br&results=10');
+    const json = await res.json();
+
+    this.setState({
+      users: json.results
+    });
+  }
+  componentDidUpdate() {
+    console.log('componentDidUpdate de app.js');
+  }
+  componentDidWillMount() {
+    console.log('componentDidWillMount de app.js');
+  }
   render() {
-    return <ProjetoBase />;
+    const { users, showUsers } = this.state;
+    return (
+      <div>
+        <div className="switch">
+          <label>
+            Mostrar usuarios
+          <input type="checkbox" />
+            <span className="lever"></span>
+          </label>
+        </div>
+        <hr />
+        <div>Users</div>
+        {/* {JSON.stringify(users)} */}
+      </div>
+    );
   }
 }
