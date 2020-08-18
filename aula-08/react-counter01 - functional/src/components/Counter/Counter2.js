dc48f784472b9322bf3ad2c5aa44def7b24e5dbb
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-17 21:20:46 -0300

    refatoração de codigo

--- aula-08/react-counter01 - functional/src/components/Counter/Counter2.js
+++ aula-08/react-counter01 - functional/src/components/Counter/Counter2.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import css from './counter.module.css';
 import IncrementButton from './IncrementButton';
 import DecrementButton from './DecrementButton';
