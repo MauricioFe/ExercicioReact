9773bca296187b9d713c61ad580f21518975bfa1
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-17 21:22:03 -0300

    steps functional

--- aula-08/react-counter01 - functional/src/components/Counter/Steps.js
+++ aula-08/react-counter01 - functional/src/components/Counter/Steps.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import css from './counter.module.css';
 
-export default class Steps extends Component {
-    render() {
-        return (
-            <span className={css.counterValue}>({this.props.steps})</span>
-        )
-    }
+export default function Steps(props) {
+    return (
+        <span className={css.counterValue}>({props.steps})</span>
+    )
 }
