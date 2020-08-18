4232229b70935b52712e825a44218aff804af798
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-17 21:38:41 -0300

    Alterando para functional

--- aula-08/react-counter01 - functional/src/components/Counter/DecrementButton.js
+++ aula-08/react-counter01 - functional/src/components/Counter/DecrementButton.js
@@ -1,9 +1,8 @@
 import React from 'react'
 
-export default function DecrementButton(props)  {
-    const handleButtonClick = () =>{
+export default function DecrementButton(props) {
+    const handleButtonClick = () => {
         props.onDecrement('-');
     }
-
-        return <button onClick={handleButtonClick} className="waves-effect waves-light btn red darken-4">-</button>
+    return <button onClick={handleButtonClick} className="waves-effect waves-light btn red darken-4">-</button>
 }
