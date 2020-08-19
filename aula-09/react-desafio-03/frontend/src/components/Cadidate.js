86fb67c0ae1f7e92332a9de6271ec9499f482498
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-19 19:02:13 -0300

    Estilização de info

--- aula-09/react-desafio-03/frontend/src/components/Cadidate.js
+++ aula-09/react-desafio-03/frontend/src/components/Cadidate.js
@@ -7,12 +7,12 @@ import Name from './Name';
 import Votes from './Votes';
 import Percentage from './Percentage';
 import Popularity from './Popularity';
-
+import css from './candidate.module.css'
 export default function Cadidate({ candidate, position }) {
     const { id, name, votes, percentage, popularity } = candidate;
     const imageSource = `${id}.jpg`
     return (
-        <div>
+        <div className={css.flexRow}>
             <Position>{position}</Position>
             <Picture imageSource={imageSource} description={name} />
             <Info>
