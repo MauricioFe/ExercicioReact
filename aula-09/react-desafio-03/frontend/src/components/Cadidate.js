a7692482d09fa4ac4874ea78fcd30d3e5c552cd7
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-19 18:30:20 -0300

    componente de picture

--- aula-09/react-desafio-03/frontend/src/components/Cadidate.js
+++ aula-09/react-desafio-03/frontend/src/components/Cadidate.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Card from './Card'
 import Position from './Position';
+import Picture from './Picture';
 
 export default function Cadidate({candidate, position}) {
-    const {name, votes} = candidate;
+    const {id, name, votes} = candidate;
+    const imageSource = `${id}.jpg`
     return (
         <div>
-           <Position>{position}</Position>{name} - {votes}
+           <Position>{position}</Position>
+           <Picture imageSource ={imageSource} description={name}/>
+           {name} - {votes}
         </div>
     )
 }
