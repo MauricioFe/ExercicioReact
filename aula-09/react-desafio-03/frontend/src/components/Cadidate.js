5fbb05dd93b17d814f20bda4b08ac1e95d7f8177
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-19 18:36:44 -0300

    Criação dos components para gerar o card

--- aula-09/react-desafio-03/frontend/src/components/Cadidate.js
+++ aula-09/react-desafio-03/frontend/src/components/Cadidate.js
@@ -3,16 +3,23 @@ import Card from './Card'
 import Position from './Position';
 import Picture from './Picture';
 import Info from './Info';
+import Name from './Name';
+import Votes from './Votes';
+import Percentage from './Percentage';
 
-export default function Cadidate({candidate, position}) {
-    const {id, name, votes} = candidate;
+export default function Cadidate({ candidate, position }) {
+    const { id, name, votes, percentage } = candidate;
     const imageSource = `${id}.jpg`
     return (
         <div>
-           <Position>{position}</Position>
-           <Picture imageSource ={imageSource} description={name}/>
-           <Info/>
-           {name} - {votes}
+            <Position>{position}</Position>
+            <Picture imageSource={imageSource} description={name} />
+            <Info>
+                <Name>{name}</Name>
+                <Votes>{votes}</Votes>
+                <Percentage>{percentage}</Percentage>
+            </Info>
+
         </div>
     )
 }
