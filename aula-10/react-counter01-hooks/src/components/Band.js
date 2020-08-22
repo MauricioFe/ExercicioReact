ba994a6dcb46755436e763d12e4abff9f883c0c4
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-21 21:11:31 -0300

    modificando o componente Counter

--- aula-10/react-counter01-hooks/src/components/Band.js
+++ aula-10/react-counter01-hooks/src/components/Band.js
@@ -1,43 +1,35 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
+const BAND_MENBERS = [
+    {
+        id: 1,
+        name: 'Neil Peart',
+        instrument: 'Bateria',
+    },
+    {
+        id: 2,
+        name: 'Alex Lifeson',
+        instrument: 'Guitarra',
+    },
+    {
+        id: 3,
+        name: 'Geddy Lee',
+        instrument: 'Baixo',
+    },
+];
+export default function Band() {
+    const [bandMembers, setBandMembers] = useState(BAND_MENBERS);
+    const [bandName, setBandName]= useState('Rush');
 
-export default class Band extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-            bandName: 'Rush',
-            bandMembers: [
-                {
-                    id: 1,
-                    name: 'Neil Peart',
-                    instrument: 'Bateria',
-                },
-                {
-                    id: 2,
-                    name: 'Alex Lifeson',
-                    instrument: 'Guitarra',
-                },
-                {
-                    id: 3,
-                    name: 'Geddy Lee',
-                    instrument: 'Baixo',
-                },
-            ]
-        }
-    }
-    render() {
-        const { bandName, bandMembers } = this.state;
-        return (
-            <div>
-                <h4>{bandName}</h4>
-                <ul>
-                    {bandMembers.map(({ id, name, instrument }) => {
-                        return (
-                            <li key={id}>{name} - {instrument}</li>
-                        );
-                    })}
-                </ul>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <h4>{bandName}</h4>
+            <ul>
+                {bandMembers.map(({ id, name, instrument }) => {
+                    return (
+                        <li key={id}>{name} - {instrument}</li>
+                    );
+                })}
+            </ul>
+        </div>
+    )
 }
