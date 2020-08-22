f708902f6c506b3263999bb2cc2e5e288a3c3914
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-22 09:19:48 -0300

    Conversão para hooks

--- aula-10/react-lifecycle-hooks/src/App.js
+++ aula-10/react-lifecycle-hooks/src/App.js
@@ -1,36 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import Users from './components/users/Users';
 import Toggle from './components/toggle/Toggle';
 
-export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      users: [],
-      showUsers: false
-    }
-  }
-  async componentDidMount() {
-    const res = await fetch('https://randomuser.me/api/?seed=rush&nat=br&results=10');
-    const json = await res.json();
+export default function App() {
+  const [users, setUsers] = useState([]);
+  const [showUsers, setShowUsers] = useState(false);
 
-    this.setState({
-      users: json.results
-    });
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const res = await fetch('https://randomuser.me/api/?seed=rush&nat=br&results=10');
+      const json = await res.json();
+      setUsers(json.results)
+    };
+    fetchUsers();
+  }, [])
+  const handleShowUsers = (isChecked) => {
+    this.setState({ showUsers: isChecked });
   }
-  handleShowUsers = (isChecked) => {
-    this.setState({ showUsers: isChecked});
-  }
-  render() {
-    const { users, showUsers } = this.state;
-    return (
-      <div>
-        <h3>React Life Cycle</h3>
-        <Toggle description="Mostrar Usuarios" enabled={showUsers} onToggle={this.handleShowUsers} />
-        <hr />
-        {/*se showUsers for true ele mostra o conteudo na div*/}
-        {showUsers && <Users users={users}/>}
-      </div>
-    );
-  }
+
+  return (
+    <div>
+      <h3>React Life Cycle</h3>
+      <Toggle description="Mostrar Usuarios" enabled={showUsers} onToggle={handleShowUsers} />
+      <hr />
+      {/*se showUsers for true ele mostra o conteudo na div*/}
+      {showUsers && <Users users={users} />}
+    </div>
+  );
 }
--- aula-10/react-lifecycle-hooks/src/components/users/Users.js
+++ aula-10/react-lifecycle-hooks/src/components/users/Users.js
@@ -1,42 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useEffect } from 'react'
 import User from './User';
 
-export default class Users extends Component {
-    constructor() {
-        super();
-        this.state = {
-            secondsVisisble: 0
-        };
-        this.interval = null;
-    }
-    componentDidMount() {
-        console.log('componentDidMount de users.js');
-        this.interval = setInterval(() => {
-            const { secondsVisisble } = this.state;
-            this.setState({
-                secondsVisisble: secondsVisisble + 1
-            });
-        }, 1000)
-    }
-    componentDidUpdate() {
-        console.log('componentDidUpdate de users.js');
-    }
-    componentWillUnmount() {
-        clearInterval(this.interval);
-    }
-    render() {
-        const { users } = this.props;
-        const { secondsVisisble } = this.state;
-        return (
-            <div>
-                <p>Componente Users visivel por {secondsVisisble}</p>
-                <ul>
-                    {users.map((user) => {
-                        const { login, name, picture } = user
-                        return <li key={login.uuid}><User user={user}/></li>
-                    })}
-                </ul>
-            </div>
-        )
-    }
+export default function Users({ users }) {
+    const [secondsVisisble, setSecondsVisible] = useState(0);
+    useEffect(() => {
+        const interval = setInterval(()=>{
+            setSecondsVisible(secondsVisisble + 1);
+        })
+        return () => {
+            clearInterval();
+        }
+    }, [secondsVisisble]);
+    return (
+        <div>
+            <p>Componente Users visivel por {secondsVisisble}</p>
+            <ul>
+                {users.map((user) => {
+                    const { login, name, picture } = user
+                    return <li key={login.uuid}><User user={user} /></li>
+                })}
+            </ul>
+        </div>
+    )
 }
