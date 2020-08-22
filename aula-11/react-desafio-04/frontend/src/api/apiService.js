ea950a52db13b33a35dbb590b459ae5d2b10f0fd
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-22 15:49:26 -0300

    Lógica para excluir dados

--- aula-11/react-desafio-04/frontend/src/api/apiService.js
+++ aula-11/react-desafio-04/frontend/src/api/apiService.js
@@ -24,16 +24,69 @@ const GRADE_VALIDATION = [
 
 async function getAllGrades() {
     const res = await axios.get(API_URL);
-    const grades = res.data.grades.map(grade =>{
-        const {student, subject, type} = grade;
-        return{
+    const grades = res.data.grades.map(grade => {
+        const { student, subject, type } = grade;
+        return {
             ...grade,
             studentLowerCase: student.toLowerCase(),
             subjectLowerCase: subject.toLowerCase(),
-            typeLoweCase: type.toLowerCase(),
-            isDeleted:false,
+            typeLowerCase: type.toLowerCase(),
+            isDeleted: false,
         };
+    });
+    let allStudents = new Set();
+    grades.forEach(grade => allStudents.add(grade.student));
+    allStudents = Array.from(allStudents);
+
+    let allSubjects = new Set();
+    grades.forEach(grade => allSubjects.add(grade.subject));
+    allSubjects = Array.from(allSubjects);
+
+    let allGradeTypes = new Set();
+    grades.forEach(grade => allGradeTypes.add(grade.type));
+    allGradeTypes = Array.from(allGradeTypes);
+    let maxId = -1;
+    grades.forEach(({id}) =>{
+        if(id > maxId){
+            maxId =id;
+        }
     })
+    let nextId = maxId + 1;
+    const allCombinations = [];
+    allStudents.forEach(student => {
+        allSubjects.forEach(subject => {
+            allGradeTypes.forEach(type => {
+                allCombinations.push({
+                    student,
+                    subject,
+                    type
+                })
+            });
+        });
+    });
+    allCombinations.forEach(({ student, subject, type }) => {
+        const hasItem = grades.find(grade => {
+            return grade.subject === subject && grade.student === student && grade.type === type;
+        });
+        if (!hasItem) {
+            grades.push({
+                id: nextId++,
+                student,
+                studentLowerCase: student.toLowerCase(),
+                subject,
+                subjectLowerCase: subject.toLowerCase(),
+                type,
+                typeLowerCase: type.toLowerCase(),
+                value: 0,
+                isDeleted: true
+            });
+        }
+    });
+
+    grades.sort((a, b) => a.typeLowerCase.localeCompare(b.typeLowerCase));
+    grades.sort((a, b) => a.subjectLowerCase.localeCompare(b.subjectLowerCase));
+    grades.sort((a, b) => a.studentLowerCase.localeCompare(b.studentLowerCase));
+
     return grades;
 }
 
